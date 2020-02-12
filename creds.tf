125c901fb424bc32ef0c0940f640d27797c2b1e2
garima-bhandari <52061481+garima-bhandari@users.noreply.github.com>
2020-02-12 12:39:39 +0530

    Create creds.tf

---
+++ creds.tf
@@ -0,0 +1,13 @@
+# AWS Config
+
+variable "aws_access_key" {
+default = "AKIAI5JLRO5MZVEFYZTQ"
+}
+
+variable "aws_secret_key" {
+default = "5RbY2HnT956IrhwbLUsabL1z+Ud9brgvC+tjs1kQ"
+}
+
+variable "aws_region" {
+default = "us-west-2"
+}
